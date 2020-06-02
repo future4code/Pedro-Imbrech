17c95d4d81cc09caad725b49d3f6cbcecd08b1a5
Pedro <elprogramadorpedro@gmail.com>
2020-06-02 20:29:32 -0300

    adicionando js

--- semana3/logica-booleana/index.js
+++ semana3/logica-booleana/index.js
@@ -21,7 +21,19 @@ console.log("d. ", resultado)
 console.log("e. ", typeof resultado)
     /*boolean*/
 
+/*-------------------------------------------------------------------------------------*/
 
+/*a. O que é `array` e como se declara em `JS`?</h1>
+<p>São variáveis que guardam mais de uma informação ao 
+mesmo tempo declaracao: var array=[1, 2, 3...]; tipo de 
+variable nome do array igual a indices começando indice cero*/
+
+/*b. Qual o index inicial de um `array`?</h1>
+ *[i]: acessa o index (começando por 0)*/
+
+/*
+c. Como se determinar o tamanho do `array`?
+O comando .length permite determinar o tamanho do array*/
 
 /*------------------------------------------------------------------------------------*/
 let array
@@ -79,10 +91,12 @@ console.log(FAHRENHEIT + " °F");
 /*c. Calcule e mostre o valor de 30°C em °F e K, mostrando as unidades no console também.*/
 
 let CENTIGRADOS = prompt("ingresa la temperatura");
-let FAHRENHEITS = (CENTIGRADOS * 9 / 5) + 32;
-let KELVINS = CENTIGRADOS + (273.15);
 
-console.log(FAHRENHEITS + " °F " + KELVINS + " K");
+
+let FAHRENHEITS = (CENTIGRADOS * 9 / 5) + 32;
+let Kelvin = CENTIGRADOS + 273.15;
+
+console.log(FAHRENHEITS + " °F " + Kelvin + " K");
 
 /*d. Altere o último item para que o usuário insira o valor em graus Celsius que ele deseja converter.*/
 
@@ -93,3 +107,27 @@ console.log("Rua ABC, 123");
 
 let color = prompt("Qual a sua cor favorita");
 console.log("Azul");
+
+let edade = prompt("qual é sua idade?");
+console.log("29anos");
+
+let linguagem = prompt("qual é a sua linguagem de programação preferida");
+console.log("javascript");
+
+let flexboxgrad = prompt("  qual você prefere flexbox ou grad ?");
+console.log("flexbox");
+
+
+/*let quantidadedeQuilowattsConsumida = prompt("quantos Quilowatt-hora voce consume");*/
+
+/*R$0.05. Faça um programa que receba a quantidade de quilowatts consumida por uma residência.*/
+
+let porcentagemdeDesconto = 15;
+let custoQuilowattHora = 0.05;
+let quantidadedeQuilowattsConsumida = 280;
+/*Calcule e mostre o valor a ser pago por uma residência que consuma 280 quilowatt-hora;*/
+/*b. Altere o programa para receber mais um valor: a porcentagem de desconto. Calcule e mostre o valor 
+a ser pago pela mesma residência acima considerando 15% de desconto.*/
+let desconto = (quantidadedeQuilowattsConsumida * custoQuilowattHora) * porcentagemdeDesconto / 100;
+let totalaPagar = (quantidadedeQuilowattsConsumida * custoQuilowattHora) - desconto;
+console.log("Total a Pagar " + totalaPagar + " R$");
