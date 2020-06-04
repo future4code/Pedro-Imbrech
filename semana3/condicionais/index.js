4181244c68236785746cec5f0ac9f655868b25af
Pedro <elprogramadorpedro@gmail.com>
2020-06-03 23:53:04 -0300

    adocionando exerxio 5

--- semana3/condicionais/index.js
+++ semana3/condicionais/index.js
@@ -1,4 +1,4 @@
-//EXERXIO 1
+/*EXERXIO 1
 
 const respostaDoUsuario = prompt("Digite o número que você quer testar?")
 const numero = Number(respostaDoUsuario)
@@ -8,6 +8,8 @@ if (numero % 2 === 0) {
 } else {
     console.log("Não passou no teste.")
 }
+*/
+
 
 /*
 Explique o que o código faz. Qual o teste que ele realiza?
@@ -21,7 +23,8 @@ Resposta:Para números ímpares *(Não passou no teste)
 */
 /*-------------------------------------------------------------------------------------------------------------------*/
 
-//EXERXIO 2
+/*
+EXERXIO-2
 
 let fruta = prompt("Escolha uma fruta")
 let preco
@@ -44,13 +47,15 @@ switch (fruta) {
             /*Break retirado*/
 
 
-    default:
-        preco = 5
-        break;
-}
+//default:
+//    preco = 5
+// break;
+//}
 
 
-console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
+//console.log("O preço da fruta ", fruta, " é ", "R$ ", preco)
+
+
 
 
 /*
@@ -82,7 +87,7 @@ Resposta: 5
 
 /*---------------------------------------------------------------------------------------------------------------*/
 
-//EXERXIO 3
+/*EXERXIO 3
 
 const numero1 = prompt("Digite o primeiro número.")
 const numero2 = prompt("Digite o próximo número?")
@@ -97,7 +102,9 @@ if (numero1 > 0 && numero2 > 0) {
 }
 
 console.log(mensagem)
-    /*
+*/
+
+/*
     Considere um usuário que digita os números 3 e 4 respectivamente.
     Qual será a mensagem do terminal? Haverá algum erro? Justifique usando
      os conceitos de bloco ou escopo.
@@ -125,3 +132,92 @@ if (numero1 > 0 && numero2 > 0) {
      */
 
 /*--------------------------------------------------------------------------------------------------------------------*/
+
+//EXERXIO 4
+/* 
+a. Crie um programa que receba dois números do usuário através do `prompt` e imprima-os na ordem **decrescente**. 
+O que acontece com o seu programa se os 2 números forem iguais? (é só testar e colocar um comentário descrevendo 
+    o que aconteceu)
+
+b. Adapte o programa para que o usuário digite 3 números.
+ Ainda os imprima na ordem **decrescente**. O que acontece como seu
+  programa se os 3 números forem iguais? (é só testar e colocar um comentário descrevendo o que aconteceu)
+
+c. Agora, impeça que o usuário digite 3 números iguais. Caso todos 
+sejam iguais, mostre um aviso ao usuário indicando que ele deve, ao menos, inserir um número diferente.*/
+
+firstNumber = Number(prompt('Digite o primeiro número'))
+secondNumber = Number(prompt('Digite o segundo número'))
+
+if (firstNumber > secondNumber && secondNumber < firstNumber) {
+    console.log(firstNumber + ' ' + secondNumber)
+
+} else {
+    console.log(secondNumber + ' ' + firstNumber)
+}
+
+/*Crie um código que imprima os números decrescentes e, se forem iguais, também os imprima.
+Eu acho que o objetivo deste exercício é que o código possa ordenar os números de maneira decrescente.*/
+
+
+
+/*----------------------------------------------------------------------------------------------------------*/
+
+
+/*c. Agora, impeça que o usuário digite 3 números iguais. Caso todos 
+sejam iguais, mostre um aviso ao usuário indicando que ele deve, ao menos, inserir um número diferente.*/
+
+firstNumber = Number(prompt('Digite o primeiro número'))
+secondNumber = Number(prompt('Digite o segundo número'))
+third = Number(prompt('Digite o segundo número'))
+
+if ((firstNumber !== secondNumber) && (firstNumber !== third) && (secondNumber !== third)) {
+    if (firstNumber > secondNumber) {
+        if (secondNumber > third) {
+            console.log(firstNumber, secondNumber, third)
+        } else if (firstNumber > third) {
+            console.log(firstNumber, third, secondNumber)
+        } else {
+            console.log(third, firstNumber, secondNumber)
+        }
+    } else {
+        if (secondNumber > third) {
+            if (firstNumber > third) {
+                console.log(secondNumber, firstNumber, third)
+            } else {
+                console.log(secondNumber, third, firstNumber)
+            }
+        } else {
+            console.log(third, secondNumber, firstNumber)
+        }
+    }
+    /*c. Agora, impeça que o usuário digite 3 números iguais. Caso todos 
+sejam iguais, mostre um aviso ao usuário indicando que ele deve, ao menos, inserir um número diferente.*/
+} else {
+    console.log('Os numeros sao iguais inserir um número diferente')
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*
+a. Escreva o diagrama esquemático que melhor represente a árvore condicional do exercício.
+ (Coloque a imagem do esquema no drive e gerem um link  de compartilhamento público. Coloque 
+    este link num comentário durante a resolução deste exercício)*/
+
+/*https://app.diagrams.net/#G1Kb2pOPjpCnNX3JMdlBGb8vez8FohcNCQ*/
+
+/*
+b. Escreva um programa que realize estas perguntas  e indique a classificação final considerada. 
+As opções são: ser humano; mamífero não humano; ave; réptil; anfíbio; peixe ou é invertebrado*/
+
+/*
+let animais=('ingrese enimal')
+if (animais==vertebrado) {
+    console.log('SER HUMANO')
+}
+else{
+    console.log('pez')
+}*/
+
+/*Preciso de um pouco mais de tempo 
+para resolver o exercício, demora um 
+pouco para entender, não terminei por falta de tempo.*/
