1e73904bf0e5d92d200ac7eb65e4770e18d4da87
Pedro <elprogramadorpedro@gmail.com>
2020-06-04 23:12:12 -0300

    adicionando exerxio4

--- semana3/loops/index.js
+++ semana3/loops/index.js
@@ -109,6 +109,9 @@ RESPOSTA:
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
 /*
 EXERCÍCIO 3
 
@@ -116,15 +119,52 @@ Nas perguntas abaixo, considere que você tenha acesso a um `array`  (chamado de
 que seja composto somente de números. Após o enunciado, há um exemplo de qual deve ser a resposta final 
 de cada programa individualmente.*/
 
-//a.Escreva um programa que devolva o maior e o menor números contidos no array original
+//a.Escreva um programa que devolva o maior e o menor números contidos no array origina
+/*
+"O maior número é 130 e o menor é 21"
+
+let array0riginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+let numeromaior = array0riginal[0];
+let numeroamenor = array0riginal[0];
+
+for (let iterator of array0riginal) {
+    if (iterator >= numeromaior) {
+        numeromaior = iterator;
+    } else if (iterator <= numeroamenor) {
+        numeroamenor = iterator;
+    }
+
+}
+
+console.log("O mayor numero é " + numeromaior + " e o menor é " + numeroamenor);
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
 
 //b. Escreva um programa que devolva um novo array contendo todos os valores do array original divididos por 10.
-let array0riginal = [180, 350, 1730, 470, 760, 241, 770, 1720, 940, 1703, 1170, 575]
+
+let array0riginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
 
 for (let divicao of array0riginal) {
     divicao /= 10
-    console.log(divicao)
+    console.log("Estes são os números  " + divicao)
 }
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
@@ -134,67 +174,41 @@ for (let divicao of array0riginal) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//c.Escreva um programa que devolva um novo array contendo, somente, os números pares do array original.
 /*
 
+let array0riginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+for (const iterator of array0riginal) {
+    if (iterator % 2 === 0) {
+        console.log(iterator)
+    } else {
+
+    }
+}
+*/
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-c.Escreva um programa que devolva um novo array contendo, somente, os números pares do array original.
 
-    d.Escreva um programa que gere um novo array contendo strings, da seguinte forma: "O elemento do índex i é: numero"
 
-Exemplos do que o código tem que fazer em cada item:
 
-    // Este array será usado para exemplificar as respostas de todos os itens
-    const array = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
 
-// Resposta a.
-"O maior número é 130 e o menor é 21"
 
-// Resposta item b.
-[8, 3, 13, 4, 6, 2.1, 7, 12, 9, 10.3, 11, 5.5]
 
-// Resposta item c.
-[80, 30, 130, 40, 60, 70, 120, 90, 110]
 
-// Resposta item d.
-['O elemento do índex 0 é 80',
-    'O elemento do índex 1 é 30',
-    'O elemento do índex 2 é 130',
-    'O elemento do índex 3 é 40',
-    'O elemento do índex 4 é 60',
-    'O elemento do índex 5 é 21',
-    'O elemento do índex 6 é 70',
-    'O elemento do índex 7 é 120',
-    'O elemento do índex 8 é 90',
-    'O elemento do índex 9 é 103',
-    'O elemento do índex 10 é 110',
-    'O elemento do índex 11 é 55'
-]*/
+
+
+
+
+
+
+//d.Escreva um programa que gere um novo array contendo strings, da seguinte forma: "O elemento do índex i é: numero"
+
+
+
+let array0riginal = [80, 30, 130, 40, 60, 21, 70, 120, 90, 103, 110, 55]
+for (const iterator of array0riginal) {
+    for (let index = 0; index < 11; index++) {}
+    console.log("O elemento do índex  0 " + " é " + iterator)
+}
