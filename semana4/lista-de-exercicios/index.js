a446f6f61cd4600d43eed345ded3055e42f240f9
Pedro <elprogramadorpedro@gmail.com>
2020-06-12 17:19:54 -0300

    Subuendo primeiros arquivos

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,4 @@
+/*Analise os trechos de código em cada exercício e escreva o que ele faz, como
+faz e qual será o valor impresso no `console`.**Importante:** A ideia é que vocês
+interpretem o código, então **não pode executar o código no JS**, tem que "executar"
+na cabeça de vocês.*/
