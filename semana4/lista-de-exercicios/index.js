fe7ec33e3ad72e0934f067ddf295a61d8d8e52cf
Pedro <elprogramadorpedro@gmail.com>
2020-06-14 14:20:49 -0300

    exercicio 1 de exercicios de interpretacao de codigo

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -1,4 +1,20 @@
 /*Analise os trechos de código em cada exercício e escreva o que ele faz, como
 faz e qual será o valor impresso no `console`.**Importante:** A ideia é que vocês
 interpretem o código, então **não pode executar o código no JS**, tem que "executar"
-na cabeça de vocês.*/
+na cabeça de vocês.
+
+
+*/
+
+
+//Exercícios de interpretação de código
+
+/*(Exercício 1)Temos uma função nomeada chamada (conversorDeMoeda) que recebe um parâmetro
+esse parâmetro tiver um nome (valorEmDolar), essa função incluirá e
+uma operação que consiste em multiplicar o (valorEmDolar) * (cotacao)
+a saída a ser impressa com o comando (console.log (meuDinheiro))
+
+valor impresso no console: Se o valor da cotação for de 5 reais, o resultado será de R$500.
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////
