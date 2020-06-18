e3d3490576f64bba710b20c438bb26547f59f2c3
Pedro <elprogramadorpedro@gmail.com>
2020-06-18 16:59:58 -0300

    melhorando insta4

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -3,32 +3,47 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+
+    state = {
+
+        Post: [
+
+            {
+                nomeUsuario: 'future4.br',
+                fotoUsuario: 'https://picsum.photos/40/40',
+                fotoPost: 'https://picsum.photos/200/150'
+            },
+            {
+                nomeUsuario: 'future4.br',
+                fotoUsuario: 'https://picsum.photos/50/50',
+                fotoPost: 'https://picsum.photos/200/150'
+            },
+            {
+                nomeUsuario: 'future4.br',
+                fotoUsuario: 'https://picsum.photos/20/20',
+                fotoPost: 'https://picsum.photos/200/150'
+            }
+
+        ]
+
+    };
+
+
     render() {
-        return ( <
-            div className = { 'app-container' } >
+        const listaDePost = this.state.Post.map((Post) => {
+            return ( <
+                div className = { 'app-container' } > { Post.nomeUsuario } - { Post.fotoUsuario } - { Post.fotoPost } <
+                /div>
+            )
+        })
+
+
+        return (
+
             <
-            div >
-            <
-            Post nomeUsuario = { 'future4.br' }
-            fotoUsuario = { 'https://picsum.photos/40/40' }
-            fotoPost = { 'https://picsum.photos/202/151' }
-            /> <
-            /div> <
-            div >
-            <
-            Post nomeUsuario = { 'future4.br' }
-            fotoUsuario = { 'https://picsum.photos/50/50' }
-            fotoPost = { 'https://picsum.photos/200/150' }
-            /> <
-            /div> <
-            div >
-            <
-            Post nomeUsuario = { 'future4.br' }
-            fotoUsuario = { 'https://picsum.photos/20/20' }
-            fotoPost = { 'https://picsum.photos/201/151' }
-            /> <
-            /div> <
+            div > { listaDePost } <
             /div>
+
         );
     }
 }
