35997a8150ef4dd0b66e7e46614662b860adf980
Pedro <elprogramadorpedro@gmail.com>
2020-06-18 22:42:49 -0300

    projeto insta4

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import Post from './components/Post/Post';
 
+
+
 class App extends React.Component {
 
     state = {
 
-        Post: [
+        postDaPessoa: [
 
             {
                 nomeUsuario: 'future4.br',
-                fotoUsuario: 'https://picsum.photos/40/40',
+                fotoUsuario: 'https://picsum.photos/50/50',
                 fotoPost: 'https://picsum.photos/200/150'
             },
             {
@@ -20,31 +22,76 @@ class App extends React.Component {
             },
             {
                 nomeUsuario: 'future4.br',
-                fotoUsuario: 'https://picsum.photos/20/20',
+                fotoUsuario: 'https://picsum.photos/50/50',
                 fotoPost: 'https://picsum.photos/200/150'
             }
 
-        ]
+        ],
 
-    };
+        valorImputNomeUsuairio: '',
+        valorImputFotoUsario: '',
+        valorImputFotoPost: ''
 
+    }
+
+    adicionaPost = () => {
+        const novoPostDaPessoa = {
+            nomeUsuario: this.state.valorImputFotoUsario,
+            fotoUsuario: this.state.valorImputFotoUsario,
+            fotoPost: this.state.valorImputFotoPost
+
+        }
+
+        const novoPost = [...this.state.postDaPessoa, novoPostDaPessoa]
+
+        this.setState({ postDaPessoa: novoPost })
+    }
+
+
+    onChangeImputNomeUsuario = event => {
+        this.setState({ valorImputFotoUsario: event.target.value })
+    }
+
+    onChangeImputFotoUsuario = event => {
+        this.setState({ valorImputFotoUsuario: event.target.value })
+    }
+
+    onChangeImputFotoPost = event => {
+        this.setState({ valorImputFotoPost: event.target.value })
+    }
 
     render() {
-        const listaDePost = this.state.Post.map((Post) => {
+
+        const postAtualizado = this.state.postDaPessoa.map((dado) => {
             return ( <
-                div className = { 'app-container' } > { Post.nomeUsuario } - { Post.fotoUsuario } - { Post.fotoPost } <
-                /div>
+                Post nomeUsuario = { dado.nomeUsuario }
+                fotoUsuario = { dado.fotoUsuario }
+                fotoPost = { dado.fotoPost }
+                />
             )
         })
 
 
-        return (
+        return ( <
+            div className = { 'app-container' } >
+            <
+            div >
 
             <
-            div > { listaDePost } <
-            /div>
-
-        );
+            input value = { this.state.valorImputNomeUsuairio }
+            onChange = { this.onChangeImputNomeUsuario }
+            placeholder = { "Nome de usuario" }
+            />  <
+            input value = { this.state.valorImputFotoUsario }
+            onChange = { this.onChangeImputFotoUsuario }
+            />  <
+            input value = { this.state.valorImputFotoPost }
+            onChange = { this.onChangeImputFotoPost }
+            /> <
+            div > { postAtualizado } < /div> <
+            /div>    <
+            /div>      
+        )
     }
 }
 
