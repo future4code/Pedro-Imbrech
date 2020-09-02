63f405a9885230a978ce59df0e0eecc8df63ea5c
Pedro <elprogramadorpedro@gmail.com>
2020-09-02 19:02:45 -0300

    exercicios de polimorfismo

---
+++ semana16/polimorfismo/template-ts/src/exercicio3.ts
@@ -0,0 +1,10 @@
+export class Industry extends Place {
+    constructor(
+      protected machinesQuantity: number, 
+      // Refere-se à quantidade de máquinas do local 
+      
+      cep: string
+          ) {
+          super(cep);
+    }
+  }
